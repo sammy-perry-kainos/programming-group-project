247aab34839e2c948d0d7cd34c48af385b714ccc
Callum <callum.esprey@kainos.com>
2023-08-24 11:25:51 +0100

    Built SalesRequest and SalesDao (up to the point SalesRequest is required)

--- src/main/java/org/kainos/ea/db/SalesDao.java
+++ src/main/java/org/kainos/ea/db/SalesDao.java
@@ -1,5 +1,23 @@
 package org.kainos.ea.db;
 
+import org.kainos.ea.cli.SalesRequest;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class SalesDao {
     DatabaseConnector databaseConnector = new DatabaseConnector();
+
+    public int createSalesEmployee(SalesRequest employee) throws SQLException {
+        Connection c = databaseConnector.getConnection();
+
+        String insertStatement = "INSERT INTO SalesEmployee (Name, Salary, BankAccountNumber, NationalInsuranceNumber, CommissionRate) " +
+                "VALUES(?, ?, ?, ?, ?)";
+
+        PreparedStatement st = c.prepareStatement(insertStatement, Statement.RETURN_GENERATED_KEYS);
+
+
+    }
 }
