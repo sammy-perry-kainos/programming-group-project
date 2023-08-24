79a78fbafe5668ff192d076e1ad53f51191e2d45
Mateen Parkar <mateenparkar4@gmail.com>
2023-08-24 11:50:32 +0100

    DeliveryReqeust class completed

--- src/main/java/org/kainos/ea/cli/DeliveryRequest.java
+++ src/main/java/org/kainos/ea/cli/DeliveryRequest.java
@@ -1,2 +1,55 @@
-package org.kainos.ea.cli;public class DeliveryRequest {
+package org.kainos.ea.cli;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class DeliveryRequest {
+    String name;
+    double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getBankNumber() {
+        return bankNumber;
+    }
+
+    public void setBankNumber(String bankNumber) {
+        this.bankNumber = bankNumber;
+    }
+
+    public String getNI() {
+        return NI;
+    }
+
+    public void setNI(String NI) {
+        this.NI = NI;
+    }
+
+    String bankNumber;
+    String NI;
+
+    @JsonCreator
+    public DeliveryRequest(@JsonProperty("name") String name, @JsonProperty("salary") double salary, @JsonProperty("bankNumber") String bankNumber, @JsonProperty("NI") String NI) {
+        this.name = name;
+        this.salary = salary;
+        this.bankNumber = bankNumber;
+        this.NI = NI;
+    }
+
+
+
 }
