d80c43ea7a4c4ba39be7e3eae5adf56f87c63fa5
Mateen Parkar <mateenparkar4@gmail.com>
2023-08-24 11:24:19 +0100

    Fix for deliveryEmployee

--- src/main/java/org/kainos/ea/cli/DeliveryEmployee.java
+++ src/main/java/org/kainos/ea/cli/DeliveryEmployee.java
@@ -1,8 +1,19 @@
 package org.kainos.ea.cli;
 
 public class DeliveryEmployee extends Employee{
-    public DeliveryEmployee(String name, Double salary, String bankNumber, String NI) {
-        super(name, salary, bankNumber, NI);
+    @Override
+    public int getEmployeeID() {
+        return EmployeeID;
+    }
+
+    @Override
+    public void setEmployeeID(int employeeID) {
+        EmployeeID = employeeID;
+    }
+
+    private int EmployeeID;
+    public DeliveryEmployee(int EmployeeID, String name, Double salary, String bankNumber, String NI) {
+        super(EmployeeID, name, salary, bankNumber, NI);
     }
 
 }
