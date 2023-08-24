5ce442d8d09c07eedbd95c6c256c51ef8b206f53
Mateen Parkar <mateenparkar4@gmail.com>
2023-08-24 10:57:53 +0100

    Getter and setter added for commision in SalesEmployee

--- src/main/java/org/kainos/ea/cli/SalesEmployee.java
+++ src/main/java/org/kainos/ea/cli/SalesEmployee.java
@@ -1,6 +1,14 @@
 package org.kainos.ea.cli;
 
 public class SalesEmployee extends Employee{
+    public Double getCommision() {
+        return commision;
+    }
+
+    public void setCommision(Double commision) {
+        this.commision = commision;
+    }
+
     private Double commision;
 
     public SalesEmployee(String name, Double salary, String bankNumber, String NI,Double commission) {
